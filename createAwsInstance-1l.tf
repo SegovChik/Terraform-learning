075a1522a8e415b67736dbfff6ea71746fe69673
segovchik <segovchikgmail.com>
2021-03-04 12:55:05 +0200

    add ansible-slaves instances

--- createAwsInstance-1l.tf
+++ createAwsInstance-1l.tf
@@ -1,12 +1,18 @@
-#terraform playbook which creates Ubuntu Server 20.04 LTS instance on AWS
-
 provider "aws" {
   access_key =  secrets.AWS_ACCESS_KEY_ID
   secret_key =  secrets.AWS_SECRET_ACCESS_KEY
   region     = "eu-central-1"
 }
 
+
 resource "aws_instance" "ansible-master" {
   ami = "ami-0767046d1677be5a0"
+  instance_type = "t2.micro"
+}
+
+
+resource "aws_instance" "ansible-slaves" {
+  count = 2                                   #number of objects to create
+  ami   = "ami-02f9ea74050d6f812"
   instance_type = "t2.micro"
 }
